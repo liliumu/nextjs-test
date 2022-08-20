35c46f75ba1693778b0a0e7db74e8c3a55ca25fb
ririumu <liliumbookair@gmail.com>
2022-08-21 00:38:56 +0900

    Top page api link

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,6 +18,12 @@ const Home: NextPage = () => {
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
+        <ul>
+          <li>
+            <a href="/api/printcow">API /api/printcow</a>
+          </li>
+        </ul>
+
         <p className={styles.description}>
           Get started by editing{" "}
           <code className={styles.code}>pages/index.tsx</code>
